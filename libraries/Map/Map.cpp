39c875ac827d4b5979e4822faa5ad93bc984d5d4
NGahman <48140671+NGahman@users.noreply.github.com>
2019-11-18 17:09:21 -0500

    Edited Map (again)

--- libraries/Map/Map.cpp
+++ libraries/Map/Map.cpp
@@ -1,26 +1,32 @@
-Map(int radius, int sphereradius)
+Map(int nodes, int sphereradius)
+//Nodes should be a perfect square for best performance, otherwise nodes rounds down to a perfect square
 {
-	r = radius;
-	map[r][r];
+	r = nodes;
+	map[r];
 	for (int i = 0; i < r; i++)
 	{
-		for (int j = 0; j < r; j++)
+			map[i] = new sphereNode();
+			map[i].index = i;
+	}
+	int c = (r / 2)+1;
+	int l = sqrt(r);
+	int h = l;
+	for (int j = -((h / 2) + 1); j < (h / 2) + 1; j++;)
+	{
+		for (int i = -((l / 2) + 1); i < (l / 2) + 1; i++;)
 		{
-			map[i][j] = new sphereNode();
-			
-			map[i][j].index = r(i) + j;
+			//Takes the relative coordinates of i and j and converts them to absolute coordinates for the array
+			map[(l * (j + (h / 2) + 1)) + (i + (l / 2) + 1)].position = (2 * i * sphereradius, 2 * j * sphereradius, 0)
 		}
 	}
+
+
 }
 
-void convertCoords(float distance, float angle, int index)
+void flush()
 {
-	float xcoord, ycoord;
-	int i, j;
-	xcoord = distance * cos(angle);
-	ycoord = distance * sin(angle);
-	i = index / r;
-	j = index % r;
-	map[i][j].position = vector3D(xcoord, ycoord, 0)
-
+	for (int i = 0; i < map.length; i++)
+	{
+		map[i].traversable = true;
+	}
 }
--- libraries/Map/Map.h
+++ libraries/Map/Map.h
@@ -1,10 +1,12 @@
 #pragma once
 #include <Arduino.h>
 #include <imumaths>
+#include <math.h>
 class Map
 {
 	int r;
 	sphereNode map;
 public:
 	Map(int radius, int sphereradius);
+	flush();
 };
